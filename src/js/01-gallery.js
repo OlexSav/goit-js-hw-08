0e2a81b3ace98fb439d9b37159dfe995b04aca84
Olexandr Savchenko <a.savchenko1207@gmail.com>
2023-10-05 16:57:31 +0300

    gallery JS/list none CSS

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+
+console.log(galleryItems);
+const pictureContainer = document.querySelector('ul.gallery');
+const cardMarkup = createPictureCardsMarup(galleryItems);
+
+pictureContainer.insertAdjacentHTML('beforeend', cardMarkup);
+
+function createPictureCardsMarup(elements) {
+  return elements
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
 
 console.log(galleryItems);
